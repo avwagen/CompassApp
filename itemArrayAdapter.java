ad3e6f3b0da7e8e9145a046a0fd9767f756ce5c6
Isaac Smith <smithi@mail.gvsu.edu>
2015-08-20 14:41:01 -0400

    Update itemArrayAdapter.java

--- itemArrayAdapter.java
+++ itemArrayAdapter.java
@@ -4,25 +4,31 @@ package com.example.aiuake4.atlascompass;
  * Created by AIUAKE4 on 8/14/2015.
  */
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Callable;
 
 public class ItemArrayAdapter extends ArrayAdapter {
     private ArrayList<String []> scoreList = new ArrayList();
-
+    private String clickedDate;
     static class ItemViewHolder {
         TextView name;
         TextView dates;
     }
 
-    public ItemArrayAdapter(Context context, int textViewResourceId) {
+    public ItemArrayAdapter(Context context, int textViewResourceId, String OCDate) {
         super(context, textViewResourceId);
+        clickedDate = OCDate;
     }
 
     //@Override
@@ -56,10 +62,21 @@ public class ItemArrayAdapter extends ArrayAdapter {
         } else {
             viewHolder = (ItemViewHolder)row.getTag();
         }
+
         String[] stat = getItem(position);
-        //This is where errors are happening
-        viewHolder.name.setText(stat[0]);
-        viewHolder.dates.setText(stat[1]);
+        String[] dog = clickedDate.split("-");
+        String month = dog[1].substring(0,3);
+        clickedDate = dog[0] + "-" + month + "-" + dog[2];
+        Log.d("test", clickedDate);
+        if(stat[0].contains(clickedDate)) {
+            Log.d("stat", stat[0]);
+            Log.d("stat1", stat[1]);
+            //Flip flop the names
+            viewHolder.name.setText(stat[0]);
+            viewHolder.dates.setText(stat[1]);
+        }else{
+            Log.d("asdf", "you messed up");
+        }
         return row;
     }
 }
